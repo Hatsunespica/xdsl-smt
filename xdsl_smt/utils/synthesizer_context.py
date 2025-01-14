c1d77d89491690f59e1350686f517248cb30c29e
spica <spicaofsky@gmail.com>
2025-01-14 00:46:18 -0800

    Add type info and more functions

--- xdsl_smt/utils/synthesizer_context.py
+++ xdsl_smt/utils/synthesizer_context.py
@@ -72,8 +72,8 @@ class Collection(Generic[T]):
             return random.choice(self.lst)
         return None
 
-    def get_all_elements(self):
-        return tuple(self.lst)
+    def get_all_elements(self) -> tuple[T]:
+        return tuple(*self.lst)
 
     def get_random_element_if(self, predicate: Callable[[T], bool]) -> T | None:
         idx = random.randint(0, self.lst_len - 1)
@@ -85,29 +85,44 @@ class Collection(Generic[T]):
         return None
 
 
-basic_int_ops = (NegOp, AndOp, OrOp, XorOp, AddOp, SubOp, SelectOp)
-full_int_ops = (
-    NegOp,
-    AndOp,
-    OrOp,
-    XorOp,
-    AddOp,
-    SubOp,
-    SelectOp,
-    LShrOp,
-    ShlOp,
-    CountLOneOp,
-    CountLZeroOp,
-    CountROneOp,
-    CountRZeroOp,
+basic_int_ops: Collection[type[Operation]] = Collection(
+    [
+        NegOp,
+        AndOp,
+        OrOp,
+        XorOp,
+        AddOp,
+        SubOp,
+        SelectOp,
+    ]
 )
-basic_i1_ops = (arith.AndI, arith.OrI, arith.XOrI, CmpOp)
+
+full_int_ops: Collection[type[Operation]] = Collection(
+    [
+        NegOp,
+        AndOp,
+        OrOp,
+        XorOp,
+        AddOp,
+        SubOp,
+        SelectOp,
+        LShrOp,
+        ShlOp,
+        CountLOneOp,
+        CountLZeroOp,
+        CountROneOp,
+        CountRZeroOp,
+    ]
+)
+basic_i1_ops: Collection[type[Operation]] = Collection(
+    [arith.AndI, arith.OrI, arith.XOrI, CmpOp]
+)
 
 
 class SynthesizerContext:
     cmp_flags: list[int]
-    i1_ops: tuple
-    int_ops: tuple
+    i1_ops: Collection[type[Operation]]
+    int_ops: Collection[type[Operation]]
 
     def __init__(self):
         self.cmp_flags = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -120,11 +135,11 @@ class SynthesizerContext:
     def use_full_int_ops(self):
         self.int_ops = full_int_ops
 
-    def get_available_i1_ops(self):
-        return self.i1_ops
+    def get_available_i1_ops(self) -> tuple[type[Operation]]:
+        return self.i1_ops.get_all_elements()
 
-    def get_available_int_ops(self):
-        return self.int_ops
+    def get_available_int_ops(self) -> tuple[type[Operation]]:
+        return self.int_ops.get_all_elements()
 
     def set_cmp_flags(self, cmp_flags: list[int]):
         assert len(cmp_flags) != 0
@@ -135,17 +150,43 @@ class SynthesizerContext:
     def get_random_i1_op(
         self, int_vals: list[SSAValue], i1_vals: list[SSAValue]
     ) -> Operation:
-        result_type = random.choice(self.i1_ops)
+        result_type = self.i1_ops.get_random_element()
         if result_type == CmpOp:
             return CmpOp(int_vals[0], int_vals[1], random.choice(self.cmp_flags))
+        assert result_type is not None
         return result_type(i1_vals[0], i1_vals[1])
 
     def get_random_int_op(
         self, int_vals: list[SSAValue], i1_vals: list[SSAValue]
     ) -> Operation:
-        result_type = random.choice(self.int_ops)
+        result_type = self.int_ops.get_random_element()
         if result_type == SelectOp:
             return SelectOp(i1_vals[0], int_vals[0], int_vals[1])
         elif result_type == NegOp:
             return NegOp(int_vals[0])
+        assert result_type is not None
+        return result_type(int_vals[0], int_vals[1])
+
+    def get_random_i1_op_except(
+        self, int_vals: list[SSAValue], i1_vals: list[SSAValue], except_op: Operation
+    ) -> Operation:
+        result_type = self.i1_ops.get_random_element_if(
+            lambda op_ty: op_ty != type(except_op)
+        )
+        if result_type == CmpOp:
+            return CmpOp(int_vals[0], int_vals[1], random.choice(self.cmp_flags))
+        assert result_type is not None
+        return result_type(i1_vals[0], i1_vals[1])
+
+    def get_random_int_op_except(
+        self, int_vals: list[SSAValue], i1_vals: list[SSAValue], except_op: Operation
+    ) -> Operation:
+        result_type = self.int_ops.get_random_element_if(
+            lambda op_ty: op_ty != type(except_op)
+        )
+        if result_type == SelectOp:
+            return SelectOp(i1_vals[0], int_vals[0], int_vals[1])
+        elif result_type == NegOp:
+            return NegOp(int_vals[0])
+        assert result_type is not None
         return result_type(int_vals[0], int_vals[1])
