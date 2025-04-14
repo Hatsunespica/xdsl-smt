31de4f59bcbe322478248fc85b2e67e758ed0e06
Yuyou Fan <spicaofsky@gmail.com>
2025-04-13 21:46:05 -0600

    Format code and move util files

--- scripts/python_utils/unsound.py
+++ scripts/python_utils/unsound.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 import os
 
-PREFIX="Skip a unsound function at bit width "
-PREFIX_LEN=len(PREFIX)
+PREFIX = "Skip a unsound function at bit width "
+PREFIX_LEN = len(PREFIX)
 
 
 def filter_file(file):
@@ -14,22 +14,24 @@ def filter_file(file):
 
     return result
 
+
 def collect_frequency(data):
     freq = {}
     for ele in data:
         if ele not in freq:
-            freq[ele]=0
-        freq[ele]+=1
+            freq[ele] = 0
+        freq[ele] += 1
     return freq
 
 
 FILE = "./data/kb_modu.log"
 
+
 def plot(file, test_name):
-    data=filter_file(file)
-    freq =collect_frequency(data)
+    data = filter_file(file)
+    freq = collect_frequency(data)
     print(freq)
-    #data=shrink_data(data,100, max)
+    # data=shrink_data(data,100, max)
     plt.subplot(1, 2, 1)
 
     bitwidth = list(freq.keys())
@@ -37,9 +39,6 @@ def plot(file, test_name):
 
     plt.bar(bitwidth, num)
 
-    plt.ylabel('Number of unsound functions')
-    plt.xlabel('Bitwdith')
+    plt.ylabel("Number of unsound functions")
+    plt.xlabel("Bitwdith")
     plt.title(os.path.basename(test_name))
-
-
-
