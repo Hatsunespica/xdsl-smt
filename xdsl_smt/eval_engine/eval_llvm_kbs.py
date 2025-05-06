42f3b0d59403a2f6a257c0f5ea7a48be48027454
dom <dominicmkennedy@gmail.com>
2025-05-06 09:58:20 -0600

    pyright

--- xdsl_smt/eval_engine/eval_llvm_kbs.py
+++ xdsl_smt/eval_engine/eval_llvm_kbs.py
@@ -1,6 +1,6 @@
 from os import path
 from subprocess import run, PIPE
-from xdsl_smt.utils.synthesizer_utils.compare_result import CompareResult
+# from xdsl_smt.utils.synthesizer_utils.compare_result import CompareResult
 
 bitwidth = 4
 
@@ -23,38 +23,39 @@ def main():
         print(eval_output.stderr, end="")
         exit(eval_output.returncode)
 
-    def get_float(s: str) -> int:
-        return eval(s)[0]
+    # def get_float(s: str) -> int:
+    #     return eval(s)[0]
 
-    def get_data(out: str):
-        eval_output_lines = out.split("\n")
-        name = eval_output_lines[0]
-        sounds = get_float(eval_output_lines[2])
-        precs = get_float(eval_output_lines[4])
-        exact = get_float(eval_output_lines[6])
-        num_cases = get_float(eval_output_lines[8])
-        unsolved_sounds = get_float(eval_output_lines[10])
-        unsolved_precs = get_float(eval_output_lines[12])
-        unsolved_exact = get_float(eval_output_lines[14])
-        unsolved_num_cases = get_float(eval_output_lines[16])
-        base_precs = get_float(eval_output_lines[18])
+    # def get_data(out: str):
+    #     eval_output_lines = out.split("\n")
+    #     name = eval_output_lines[0]
+    #     sounds = get_float(eval_output_lines[2])
+    #     precs = get_float(eval_output_lines[4])
+    #     exact = get_float(eval_output_lines[6])
+    #     num_cases = get_float(eval_output_lines[8])
+    #     unsolved_sounds = get_float(eval_output_lines[10])
+    #     unsolved_precs = get_float(eval_output_lines[12])
+    #     unsolved_exact = get_float(eval_output_lines[14])
+    #     unsolved_num_cases = get_float(eval_output_lines[16])
+    #     base_precs = get_float(eval_output_lines[18])
+    #
+    #     return name, CompareResult(
+    #         num_cases,
+    #         sounds,
+    #         exact,
+    #         precs,
+    #         unsolved_num_cases,
+    #         unsolved_sounds,
+    #         unsolved_exact,
+    #         unsolved_precs,
+    #         base_precs,
+    #         bitwidth,
+    #     )
 
-        return name, CompareResult(
-            num_cases,
-            sounds,
-            exact,
-            precs,
-            unsolved_num_cases,
-            unsolved_sounds,
-            unsolved_exact,
-            unsolved_precs,
-            base_precs,
-            bitwidth,
-        )
-
-    eval_outputs = eval_output.stdout.split("---\n")
-    results = [get_data(x) for x in eval_outputs if x != ""]
-    [print(f"{name:<10}", cp.get_exact_prop()) for name, cp in results]
+    eval_output.stdout.split("---\n")
+    # eval_outputs = eval_output.stdout.split("---\n")
+    # results = [get_data(x) for x in eval_outputs if x != ""]
+    # [print(f"{name:<10}", cp.get_exact_prop()) for name, cp in results]
 
 
 if __name__ == "__main__":
