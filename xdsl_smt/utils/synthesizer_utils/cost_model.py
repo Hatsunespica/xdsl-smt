bd665fbdba1ca5e58d608f3652f456bac885ff4f
DOFY <dofypxy@gmail.com>
2025-06-02 20:39:10 +0000

    unify the format of cost function

--- xdsl_smt/utils/synthesizer_utils/cost_model.py
+++ xdsl_smt/utils/synthesizer_utils/cost_model.py
@@ -3,34 +3,37 @@ import math
 from xdsl_smt.utils.synthesizer_utils.compare_result import EvalResult
 
 
-def sound_and_precise_cost(res: EvalResult) -> float:
-    a = 1
-    b = 16
+def general_cost(a: float, b: float, s: float, p: float) -> float:
+    return (a * (1 - s) + b * (1 - p)) / (a + b)
+
+
+def general_sound_and_dist_cost(a: float, b: float, res: EvalResult) -> float:
     sound = res.get_sound_prop()
     dis = res.get_sound_dist() / res.get_base_dist()
-    # if dis == 1:
-    #     return 1
-    return (a * (1 - sound) + b * dis) / (a + b)
+    return general_cost(a, b, sound, 1 - dis)
 
 
-def precise_cost(res: EvalResult) -> float:
-    a = 0
-    b = 1
+def _more_sound(res: EvalResult) -> float:
+    return general_sound_and_dist_cost(1, 16, res)
+
+
+def _only_precise(res: EvalResult) -> float:
+    return general_sound_and_dist_cost(0, 1, res)
+
+
+def _less_sound(res: EvalResult) -> float:
+    return general_sound_and_dist_cost(1, 2, res)
+
+
+def _sound_first(res: EvalResult) -> float:
     sound = res.get_sound_prop()
-    dis = res.get_sound_dist() / res.get_base_dist()
-    # if dis == 1:
-    #     return 1
-    return (a * (1 - sound) + b * dis) / (a + b)
+    dis = res.get_sound_dist() / res.get_base_dist() if sound == 1 else 1
+    return general_cost(1, 1, sound, 1 - dis)
 
 
-def abduction_cost(res: EvalResult) -> float:
-    a = 1
-    b = 2
-    sound = res.get_sound_prop()
-    dis = res.get_sound_dist() / res.get_base_dist()
-    # if dis == 1:
-    #     return 1
-    return (a * (1 - sound) + b * dis) / (a + b)
+sound_and_precise_cost = _more_sound
+precise_cost = _only_precise
+abduction_cost = _less_sound
 
 
 def decide(p: float, beta: float, current_cost: float, proposed_cost: float) -> bool:
